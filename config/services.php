413b3e198d4d04fdfdc5635a1353df956c972118
Gustavo Costa Arakaki <gustavo.costarakaki@gmail.com>
2024-09-27 11:03:55 -0300

    LoginUsp

--- config/services.php
+++ config/services.php
@@ -34,5 +34,13 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    
+    'senhaunica' => [
+    	'client_id' => env('SENHAUNICA_KEY'),
+    	'client_secret' => env('SENHAUNICA_SECRET'),
+    	'callback_id' => env('SENHAUNICA_CALLBACK_ID'),
+    	'dev' => env('SENHAUNICA_DEV', 'no'),
+    	'redirect' => '/',
+    ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\PlansController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\LoginController;
+
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+  return Inertia::render('Home');
 });
+#Route::get('/', [IndexController::class, 'index']);
+Route::get('login', [LoginController::class, 'redirectToProvider']);
+Route::get('callback', [LoginController::class, 'handleProviderCallback']);
+Route::get('logout', [LoginController::class, 'logout']);
+
 
 Route::get('/test', [PlansController::class, 'test']);
