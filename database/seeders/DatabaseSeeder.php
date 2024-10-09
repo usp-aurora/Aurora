3053e331305817fb9fc7ae5ca6a50c4bbfb1bb95
Kim <kimkakeya@gmail.com>
2024-10-09 15:08:41 -0300

    feat: ajustando a execução dos seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\SubjectSeeder;
+use Database\Seeders\PlanSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +20,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            SubjectSeeder::class,
+            PlanSeeder::class
         ]);
     }
 }
