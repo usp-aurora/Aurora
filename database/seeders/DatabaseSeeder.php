4128ddf80e269407ea3cf9419255b8e9e240416e
Kim <kimkakeya@gmail.com>
2024-12-20 21:33:25 -0300

    fix: ajuste no seed de database para incluir um usuário inicial

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Teste',
+            'email' => 'teste@teste.com',
+            'codpes' => '12345678'
+        ]);
 
         $this->call([
             SubjectSeeder::class,
