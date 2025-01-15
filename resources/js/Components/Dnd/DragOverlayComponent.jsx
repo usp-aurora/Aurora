f4bdfb7424e3b9ec0f3f837ffeef7b22180e66b1
Giovanna <gio-uchoa@hotmail.com>
2025-01-15 17:09:26 -0300

    refactor para otimizar drag handlers

--- resources/js/Components/Dnd/DragOverlayComponent.jsx
+++ resources/js/Components/Dnd/DragOverlayComponent.jsx
@@ -3,23 +3,19 @@ import { restrictToWindowEdges } from '@dnd-kit/modifiers';
 import Card from '../Atoms/Card';
 import CardContentCourse from '../Atoms/CardContentCourse';
 
-const DragOverlayComponent = ({ dragObject }) => {
-    const CourseOverlay = ({course}) => {
-        return (  
-            <Card colors={course.colors}>
-                <CardContentCourse 
-                  pokeball={course.pokeball} 
-                  courseCode={course.code} 
-                  courseTitle={course.title} 
-                  pokemonURL="/pokemons/ditto.png"
-                />
-            </Card> 
-        );
-    } 
-  
+const DragOverlayComponent = ({ course }) => {
     return (
         <DragOverlay modifiers={[restrictToWindowEdges]}>
-            {dragObject ? <CourseOverlay course={dragObject.course} /> : null}
+            {course ? 
+                <Card colors={course.colors}>
+                    <CardContentCourse 
+                        pokeball={course.pokeball} 
+                        courseCode={course.code} 
+                        courseTitle={course.title} 
+                        pokemonURL="/pokemons/ditto.png"
+                    />
+                </Card> 
+            : null}
         </DragOverlay>
     );
 };
