d4a8f6d29b9a7a89acd382a575b4218dbd336471
peo5 <pedromsimoesferreira@gmail.com>
2025-01-22 21:43:43 -0300

    fix: revertida estilização dinâmica com styled components

--- resources/js/Components/GraphView/NodeView.jsx
+++ resources/js/Components/GraphView/NodeView.jsx
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 const NodeContainerView = styled.div`
 	position: absolute;
-    transform: ${({ xTranslation, yTranslation }) => `translate(${xTranslation}px, ${yTranslation}px) translate(-50%, -50%)`};
 `
 
 function NodeView({x=0,y=0,onMouseDown,children}) {
 	return (
 		<NodeContainerView
-			xTranslation={x}
-			yTranslation={y}
+			style={{
+				transform: `translate(${x}px, ${y}px) translate(-50%, -50%)`
+			}}
 			onMouseDown={onMouseDown}
 		>
 			{children}
