4e4fbbd0ad1252fc4be43216749b13d00ba89339
Kim <kimkakeya@gmail.com>
2025-02-22 18:14:56 -0300

    feat: Componente para a logo

--- resources/js/Components/Header/Header.jsx
+++ resources/js/Components/Header/Header.jsx
@@ -2,30 +2,19 @@ import React from 'react';
 import { Typography } from '@mui/material/';
 import { styled } from "@mui/material/styles";
 
+import Logo from '../Atoms/Logo/Logo';
+
 const HeaderContainer = styled("div")({
     width: "100%",
     display: "flex",
     justifyContent: "spaceBetween", 
 });
 
-const ShiningText = styled(Typography)(({ theme }) => ({
-    background: `linear-gradient(90deg, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
-    backgroundSize: '200% 200%',
-    animation: 'shining 3s linear infinite',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-
-    '@keyframes shining': {
-        '0%': { backgroundPosition: '0% 50%' },
-        '50%': { backgroundPosition: '100% 50%' },
-        '100%': { backgroundPosition: '0% 50%' },
-    },
-}));
 
 const Header = () => {
     return (
         <HeaderContainer>
-            <ShiningText variant="h1">Aurora</ShiningText>
+            <Logo />
         </HeaderContainer>
     );
 };
