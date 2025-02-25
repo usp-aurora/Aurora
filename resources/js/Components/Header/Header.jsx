39876211755fcfb57024c7dc5ce518c5ab5823e2
Kim <kimkakeya@gmail.com>
2025-02-25 15:22:45 -0300

    feat: header com botão de login (ainda não funcional)

--- resources/js/Components/Header/Header.jsx
+++ resources/js/Components/Header/Header.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Typography } from '@mui/material/';
+import { Box, Stack, Typography } from '@mui/material/';
 import { styled } from "@mui/material/styles";
 
 import Logo from '../Atoms/Logo/Logo';
+import Button from '../Atoms/Buttons/Button';
 
-const HeaderContainer = styled("div")({
+const HeaderContainer = styled(Box)({
     width: "100%",
     display: "flex",
-    justifyContent: "spaceBetween", 
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "24px",
 });
 
+const LoginText = styled(Typography)(({ theme }) => ({
+    ...theme.typography.small,
 
-const Header = () => {
+    [theme.breakpoints.up("sm")]: {
+        ...theme.typography.p,
+    }
+}));
+
+
+const Header = (props) => {
     return (
-        <HeaderContainer>
+        <HeaderContainer {...props}>
             <Logo />
+            <Stack spacing={4} direction="row" alignItems="center">
+                <LoginText>Entre com seu e-mail USP para salvar o progresso </LoginText>
+                <Button size="Large"> Entrar </Button>
+            </Stack>
         </HeaderContainer>
     );
 };
