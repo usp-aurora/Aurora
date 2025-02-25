ebea285ae2cc95abb43b808497fc79cf9b9589d6
Kim <kimkakeya@gmail.com>
2025-02-25 15:23:04 -0300

    refactor: pequeno ajude no arquivo da Logo

--- resources/js/Components/Atoms/Logo/Logo.jsx
+++ resources/js/Components/Atoms/Logo/Logo.jsx
@@ -1,8 +1,8 @@
 import { Typography } from '@mui/material/';
 import { styled } from "@mui/material/styles";
-const LogoText = styled(Typography)({
-    textShadow: '0px 0px 16px rgba(255, 255, 255, 0.5)'
-});
+const LogoText = styled(Typography)(({}) => ({
+    textShadow: '0px 0px 16px rgba(255, 255, 255, 0.5)',
+}));
 
 function Logo(props){
     return (
