602f6a3888d8697401b93f263ba4d260e7c91e20
Kim <kimkakeya@gmail.com>
2025-04-06 13:42:21 -0300

    fix: ajustes nas rotas para melhorar segurança de dados

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\App;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/api/subject/{subjectCode}', [HomeController::class, 'getSubjectRequirements']);
 
-Route::get('login', [LoginController::class, 'redirectToProvider']);
-Route::get('callback', [LoginController::class, 'handleProviderCallback']);
-Route::get('logout', [LoginController::class, 'logout']);
+Route::get('login', function () {
+	abort(404);
+});
 
-Route::get('/teste_subject', [TestController::class, 'subject']);
-Route::get('/teste_requirement', [TestController::class, 'requirement']);
-Route::get('/teste_sr', [TestController::class, 'subjectWithRequirements']);
+if (!App::environment('production')) {
+	Route::get('/teste_subject', [TestController::class, 'subject']);
+	Route::get('/teste_requirement', [TestController::class, 'requirement']);
+	Route::get('/teste_sr', [TestController::class, 'subjectWithRequirements']);
+}
