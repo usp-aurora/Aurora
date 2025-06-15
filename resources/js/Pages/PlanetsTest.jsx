ad8406a96d35acad06a2324b4f015b241535659a
Kim <kimkakeya@gmail.com>
2025-06-15 16:03:44 -0300

    feat: novo componente de planetas e uma página para testar os planetas

--- resources/js/Pages/PlanetsTest.jsx
+++ resources/js/Pages/PlanetsTest.jsx
@@ -11,6 +11,29 @@ const AppContainer = styled(Box)(({ theme }) => ({
 	justifyContent: "center",
 }));
 
+const colors = [
+	"blue",
+	"brown",
+	"cyan",
+	"green",
+	"orange",
+	"pink",
+	"purple",
+	"red",
+	"yellow",
+];
+
+const seeds = [
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+	Math.random().toString(36).substring(2, 10),
+]
 
 function PlanetsTest() {
 	return (
@@ -18,8 +41,32 @@ function PlanetsTest() {
 			<Typography variant="h2">
 				Planeta teste
 			</Typography>
-			<Box sx={{ width: "40%", height: "40%" }}>
-				<Planet />
+			<Stack direction="row" spacing={2}>
+				{
+					colors.map((color, i) => (
+						<Box key={i} sx={{ width: "50px" }}>
+							<Planet color={color} seed={seeds[i]} />
+						</Box>
+					))
+				}
+			</Stack>
+			<Box sx={{ width: "100%", overflowX: "auto" }}>
+				<Stack
+					direction="row"
+					spacing={2}
+					sx={{
+						width: `${colors.length * 500 + (colors.length - 1) * 16}px`, // 16px = 2 * 8px (default spacing unit)
+						minWidth: "max-content",
+					}}
+				>
+					{
+						colors.map((color, i) => (
+							<Box key={i} sx={{ width: "500px" }}>
+								<Planet color={color} seed={seeds[i]} />
+							</Box>
+						))
+					}
+				</Stack>
 			</Box>
 		</AppContainer>
 	);
--- resources/js/Planets/Components/Planet.jsx
+++ resources/js/Planets/Components/Planet.jsx
@@ -1,32 +1,34 @@
 import { Box } from '@mui/material';
 import PlanetView from '../views/PlanetView.mjs';
+import { useTheme } from '@mui/material/styles';
+
 
 const baseTemplate = {
-  "seed": "mac031212311231250",
+  "seed": "mac0110",
   "resolution": 3,
-  "color": "rgb(80,120,180)",
+  "color": "#095C4D",
   "noises": {
     "a": {
-      "seed-suffix": "noise A",
+      "seed-suffix": "a",
       "octave-strengths": [
-        100,
-        5,
+        10,
+        7,
         1
       ]
     },
     "b": {
       "octave-strengths": [
         1,
-        5,
-        20
+        7,
+        90
       ],
       "seed-suffix": "b"
     },
     "clouds": {
       "octave-strengths": [
-        2,
         1,
-        5
+        1,
+        3
       ],
       "seed-suffix": "clouds"
     }
@@ -34,44 +36,59 @@ const baseTemplate = {
   "layers": [
     {
       "noise-strengths": {
-        "a": 2,
+        "a": 1,
         "b": 1
       },
-      "span": 0.45,
-      "color": "rgb(5, 219, 79)"
+      "span": 0.26,
+      "color": "#15B48F"
     },
     {
       "noise-strengths": {
         "a": 1,
-        "b": 2
+        "b": 1
       },
-      "span": 0.15,
-      "color": "rgb(226, 60, 0)"
+      "span": 0.12,
+      "color": "#72E3C1"
     }
   ],
   "clouds": {
     "noise-strengths": {
       "clouds": 1
     },
-    "span": 0.3,
-    "color": "rgb(39, 138, 0)",
-    "height": 5
+    "span": 0.01,
+    "color": "rgb(255, 255, 255)",
+    "height": 10
   },
   "atmosphere": {
-    "color": "rgb(101, 255, 40)",
-    "height": 18
+    "color": "rgb(255, 255, 255)",
+    "height": 25
   }
 }
 
-function generatePlanet(color, seed) {
-	const template = {
-		...baseTemplate,
-		color: color || baseTemplate.color,
-		seed: seed || baseTemplate.seed
-	};
+function generatePlanet(seed, seaColor, landOuterColor, landInnerColor, cloudsColor, hasClouds) {
+  const template = {
+    ...baseTemplate,
+    seed: seed || baseTemplate.seed,
+    color: seaColor || baseTemplate.color,
+    layers: [
+      {
+        ...baseTemplate.layers[0],
+        color: landOuterColor || baseTemplate.layers[0].color
+      },
+      {
+        ...baseTemplate.layers[1],
+        color: landInnerColor || baseTemplate.layers[1].color
+      }
+    ],
+    clouds: {
+      ...baseTemplate.clouds,
+      "noise-strengths": {
+        clouds: hasClouds ? 1 : 0
+      },
+      color: cloudsColor || baseTemplate.clouds.color
+    }
 
-	console.log('Template being used:', template);
-	console.log('Cloud configuration:', template.clouds);
+  };
 
 	try {
 		const planet = new PlanetView(template);
@@ -87,11 +104,20 @@ function generatePlanet(color, seed) {
 	}
 }
 
-function Planet({ dummy }) {
-	let color = "rgb(242, 19, 194)";
-	let seed = "mac0320";
+function Planet({ seed="default", color="blue" }) {
+  const theme = useTheme();
+  let colorPalette = theme.palette[color];
+  let seaColor = colorPalette[900];
+  let landOuterColor = colorPalette[600];
+  let landInnerColor = colorPalette[300];
 
-	const planetSVG = generatePlanet(color, seed);
+  const seedToValue = [200, 300, 400, 500];
+  let value = seedToValue[Math.abs(seed.split('').reduce((acc, c) => acc + c.charCodeAt(0), 0)) % seedToValue.length];
+  let cloudsColor = theme.palette["grey"][value];
+
+  let hasClouds = ["green", "blue", "cyan"].includes(color);
+
+	const planetSVG = generatePlanet(seed, seaColor, landOuterColor, landInnerColor, cloudsColor, hasClouds);
 
 	return (
 		<Box
