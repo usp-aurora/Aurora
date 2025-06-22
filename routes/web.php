11f796ae5e85d2414ce8c490e441d079a5c2dc1d
Kim <kimkakeya@gmail.com>
2025-06-22 02:17:54 -0300

    fix: arrumando testes dos models

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,12 @@ use App\Http\Controllers\PlanController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\TestController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware('auth')->prefix('api/plans')->group(function () {
     Route::get('/index', [PlanController::class, 'index']);
     Route::post('/sync', [PlanController::class, 'sync']);
-    // Route::post('', [PlanController::class, 'store']);
-    // Route::put('/{id}', [PlanController::class, 'update']);
-    // Route::delete('/{id}', [PlanController::class, 'destroy']);
 });
 
 Route::get('/export', [PlanController::class, 'export']);
